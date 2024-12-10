1b5522f7d7c3903ad64be772a5608377911fa2ef
Sandeep <sandeepnegi686@gmail.com>
2024-12-10 11:55:52 +0530

    added some functionality.

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -131,6 +131,8 @@ export async function getBookedDatesByCabinId(cabinId) {
 
 export async function getSettings() {
   const { data, error } = await supabase.from("settings").select("*").single();
+  //For testing
+  // await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);
