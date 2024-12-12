375e702be905c58c0d9831db2a915a6b621533da
Sandeep <sandeepnegi686@gmail.com>
2024-12-12 12:33:07 +0530

    added auth functionallity in next js project

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export const {
+  auth,
+  handlers: { GET, POST },
+} = NextAuth(authOptions);
