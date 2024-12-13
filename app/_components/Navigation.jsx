1296405062b11fb29ddbe4afcefbf96a3dca8d48
Sandeep <sandeepnegi686@gmail.com>
2024-12-13 12:19:19 +0530

    added some features of auth.js

--- app/_components/Navigation.jsx
+++ app/_components/Navigation.jsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "@/app/api/auth/[...nextauth]/route";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+  // console.log(user);
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +25,29 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.name ? (
+            <div className="flex gap-4 items-center">
+              <img
+                src={session?.user?.image}
+                alt="profile"
+                style={{ width: "25px", height: "25px", borderRadius: "50%" }}
+                referrerPolicy="no-referrer"
+              />
+              <Link
+                href="/account"
+                className="hover:text-accent-400 transition-colors"
+              >
+                {session?.user?.name}
+              </Link>
+            </div>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
